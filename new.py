23dc5d88384464ba34e14e5882913edeba8afeb2
AsifIqbal74 <hasifiqbal"yahoo.com>
2024-07-04 12:25:06 +0800

    New print addedd

---
+++ new.py
@@ -0,0 +1,87 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "3c9d0a35",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Asif iqbal\n",
+      "with Allah help i made it\n",
+      "will Continue, In Sha Allah\n",
+      " going to fetch isma\n",
+      "I am improving\n"
+     ]
+    }
+   ],
+   "source": [
+    "print ('Asif iqbal')\n",
+    "print('with Allah help i made it')\n",
+    "print('will Continue, In Sha Allah')\n",
+    "print(' going to fetch isma')\n",
+    "print('I am improving')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "1df3df56",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Asif iqbal\n",
+      "with Allah help i made it\n",
+      "will Continue, In Sha Allah\n",
+      " going to fetch isma\n",
+      "I am improving\n",
+      "Am i getting the grip\n"
+     ]
+    }
+   ],
+   "source": [
+    "print ('Asif iqbal')\n",
+    "print('with Allah help i made it')\n",
+    "print('will Continue, In Sha Allah')\n",
+    "print(' going to fetch isma')\n",
+    "print('I am improving')\n",
+    "print('Am i getting the grip')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9389e95b",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
